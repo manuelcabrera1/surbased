776e3f5629405d5777ee97b5225159bbdcac123d
Manuel <i12cacrm@uco.es>
2025-03-10 21:25:37 +0100

    refactor: add 'updated_at' to survey_participant model

--- api/src/models/SurveyParticipantModel.py
+++ api/src/models/SurveyParticipantModel.py
@@ -1,10 +1,14 @@
+from datetime import datetime
 from database import Base
-from sqlalchemy import Column, ForeignKey, Table
+from sqlalchemy import Column, DateTime, ForeignKey, Table
 
 survey_participant = Table(
     "survey_participant",
     Base.metadata,
     Column("participant_id", ForeignKey("users.id"), primary_key=True, index=True),
-    Column("survey_id", ForeignKey("surveys.id"), primary_key=True, index=True)
+    Column("survey_id", ForeignKey("surveys.id"), primary_key=True, index=True),
+    Column("updated_at", DateTime, default=datetime.now)
+
+    
 )
 
