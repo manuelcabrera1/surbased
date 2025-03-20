df99719b0bb4a5ccac621d752d0a5cc1b1500264
Manuel <i12cacrm@uco.es>
2025-03-20 18:11:53 +0100

    fix: change organization endpoints to check current user

--- api/src/routes/organization/OrganizationController.py
+++ api/src/routes/organization/OrganizationController.py
@@ -14,36 +14,44 @@ from auth.Auth import create_access_token, check_current_user, get_current_user,
 org_router = APIRouter(tags=["Organization"])
 
 @org_router.post("/orgs/create", status_code=201, response_model=OrganizationResponse)
-async def create_organization(org: OrganizationCreate, db: Annotated[AsyncSession, Depends(get_db)]):
+async def create_organization(org: OrganizationCreate, db: Annotated[AsyncSession, Depends(get_db)], current_user: Annotated[User, Depends(get_current_user)] = None):
 
-        result = await db.execute(select(Organization).where(Organization.name == org.name))
-        existing_org = result.unique().scalars().first()
+    if not current_user:
+        raise HTTPException(status_code=401, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+    
+    result = await db.execute(select(Organization).where(Organization.name == org.name))
+    existing_org = result.unique().scalars().first()
 
-        if existing_org:
-            raise HTTPException(status_code=400, detail="This organization already exists")
+    if existing_org:
+        raise HTTPException(status_code=400, detail="This organization already exists")
 
-        new_org = Organization(name=org.name)
-        db.add(new_org)
-        await db.commit()
-        await db.refresh(new_org)
+    new_org = Organization(name=org.name)
+    db.add(new_org)
+    await db.commit()
+    await db.refresh(new_org)
 
-        return new_org
+    return new_org
 
 
  
-@org_router.get("/orgs", status_code=200, response_model=OrganizationResponseWithLength, dependencies=[Depends(check_current_user)])
+@org_router.get("/orgs", status_code=200, response_model=OrganizationResponseWithLength)
 @required_roles(["admin"])
-async def get_all_organizations(db: Annotated[AsyncSession, Depends(get_db)]):
+async def get_all_organizations(db: Annotated[AsyncSession, Depends(get_db)], current_user: Annotated[User, Depends(get_current_user)] = None):
 
+        if not current_user:
+            raise HTTPException(status_code=401, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+        
         result = await db.execute(select(Organization))
         orgs = result.unique().scalars().all()
         return { "orgs": orgs, "length": len(orgs) }
     
 
 
-@org_router.get("/orgs/{id}", status_code=200, response_model=OrganizationResponse, dependencies=[Depends(check_current_user)])
-async def get_organization_by_id(id:uuid.UUID, db: Annotated[AsyncSession, Depends(get_db)]):
+@org_router.get("/orgs/{id}", status_code=200, response_model=OrganizationResponse)
+async def get_organization_by_id(id:uuid.UUID, db: Annotated[AsyncSession, Depends(get_db)], current_user: Annotated[User, Depends(get_current_user)] = None):
 
+        if not current_user:
+            raise HTTPException(status_code=401, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
         
         result = await db.execute(select(Organization).where(Organization.id == id))
         existing_org = result.unique().scalars().first()
@@ -53,25 +61,24 @@ async def get_organization_by_id(id:uuid.UUID, db: Annotated[AsyncSession, Depen
         
         return existing_org
 
-@org_router.get("/orgs/{id}/users", status_code=200, response_model=UserResponseWithLength, dependencies=[Depends(check_current_user)])
-@required_roles(["researcher"])
+@org_router.get("/orgs/{id}/users", status_code=200, response_model=UserResponseWithLength)
+@required_roles(["researcher", "admin"])
 async def get_all_users_in_organization(id:uuid.UUID, db: Annotated[AsyncSession, Depends(get_db)], 
                                         current_user: Annotated[User, Depends(get_current_user)] = None, 
-                                        sortBy: Optional[str] = 'name', order: Optional[str] = 'ASC'):
+                                        sortBy: Optional[str] = 'email', order: Optional[str] = 'ASC'):
         
         if not current_user:
             raise HTTPException(status_code=401, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
         
-        if current_user.organization_id != id:
+        if current_user.organization_id != id and current_user.role != "admin":
             raise HTTPException(status_code=403, detail="You are not allowed to access this organization")
         
 
         result = await db.execute(select(User).where(User.organization_id == id))
 
         users = result.unique().scalars().all()
-        if sortBy == "name":
-            users.sort(key=lambda a: (a.name), reverse=order == 'DESC')
-        elif sortBy == "email":
+
+        if sortBy == "email":
             users.sort(key=lambda a: (a.email), reverse=order == 'DESC')
         elif sortBy == "role":
             users.sort(key=lambda a: (a.role), reverse=order == 'DESC')
@@ -80,10 +87,12 @@ async def get_all_users_in_organization(id:uuid.UUID, db: Annotated[AsyncSession
             
     
 
-@org_router.put("/orgs/{id}", status_code=201, response_model=OrganizationResponse, dependencies=[Depends(check_current_user)])
+@org_router.put("/orgs/{id}", status_code=201, response_model=OrganizationResponse)
 @required_roles(["admin"])
-async def update_organization(id: uuid.UUID, org: OrganizationCreate, db: Annotated[AsyncSession, Depends(get_db)]):
+async def update_organization(id: uuid.UUID, org: OrganizationCreate, db: Annotated[AsyncSession, Depends(get_db)], current_user: Annotated[User, Depends(get_current_user)] = None):
 
+        if not current_user:
+            raise HTTPException(status_code=401, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
         
         #check if org exists
         result = await db.execute(select(Organization).where(Organization.id == id))
@@ -109,10 +118,13 @@ async def update_organization(id: uuid.UUID, org: OrganizationCreate, db: Annota
 
 
 
-@org_router.delete("/orgs/{id}", status_code=204, dependencies=[Depends(check_current_user)])
+@org_router.delete("/orgs/{id}", status_code=204)
 @required_roles(["admin"])
-async def delete_organization(id: uuid.UUID, db: Annotated[AsyncSession, Depends(get_db)]):
+async def delete_organization(id: uuid.UUID, db: Annotated[AsyncSession, Depends(get_db)], current_user: Annotated[User, Depends(get_current_user)] = None):
 
+        if not current_user:
+            raise HTTPException(status_code=401, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+        
         #check if org exists
         result = await db.execute(select(Organization).where(Organization.id == id))
         existing_org = result.unique().scalars().first()
