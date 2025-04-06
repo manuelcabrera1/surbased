a16611bf4bafdd7d37201572e0552137e085f60f
Manuel <i12cacrm@uco.es>
2025-04-06 12:53:46 +0200

    refactor(frontend): remove some unused imports

--- client/surbased/lib/src/user/application/pages/user_create_page.dart
+++ client/surbased/lib/src/user/application/pages/user_create_page.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:surbased/src/auth/application/provider/auth_provider.dart';
-import 'package:surbased/src/auth/application/widgets/date_form_field_widget.dart';
-import 'package:surbased/src/config/app_routes.dart';
-import 'package:surbased/src/survey/application/provider/survey_provider.dart';
-import 'package:surbased/src/survey/application/widgets/survey_form.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:surbased/src/user/application/widgets/user_form.dart';
-import '../../../category/application/provider/category_provider.dart';
 
 
 class UserCreatePage extends StatefulWidget {
