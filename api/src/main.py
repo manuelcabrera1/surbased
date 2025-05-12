a66d70845f1917bf46e25c683bf802f22cf7eade
Manuel <i12cacrm@uco.es>
2025-05-12 20:25:40 +0200

    refactor(backend): remove option and questions logic

--- api/src/main.py
+++ api/src/main.py
@@ -8,9 +8,6 @@ from routes.surveyusers.SurveyUsersController import survey_users_router
 from routes.organization.OrganizationController import org_router
 from routes.category.CategoryController import category_router
 from routes.survey.SurveyController import survey_router
-from routes.metric.MetricController import metric_router
-from routes.question.QuestionController import question_router
-from routes.option.OptionController import option_router
 from routes.answer.AnswerController import answer_router
 from routes.mailing.MailController import mail_router
 from routes.tag.TagController import tag_router
@@ -36,9 +33,6 @@ app.include_router(org_router)
 app.include_router(category_router)
 app.include_router(survey_router)
 app.include_router(survey_users_router)
-app.include_router(metric_router)
-app.include_router(question_router)
-app.include_router(option_router)
 app.include_router(answer_router)
 app.include_router(mail_router)
 app.include_router(tag_router)
